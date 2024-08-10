9f693749c17a401f77905ff2c34174619b289e26
Kenichi Kamiya <kachick1@gmail.com>
2024-08-10 18:38:51 +0900

    Set update_url as official instruction
    https://github.com/dprint/plugins#redirect-to-any-github-repo-for-plugin-authors

--- src/lib.rs
+++ src/lib.rs
@@ -65,7 +65,9 @@ impl SyncPluginHandler<Configuration> for KdlPluginHandler {
                 config_key: "kdl".to_string(),
                 help_url: "https://github.com/kachick/dprint-plugin-kdl".to_string(), // fill this in
                 config_schema_url: "".to_string(), // leave this empty for now
-                update_url: None,                  // leave this empty for now
+                update_url: Some(
+                    "https://plugins.dprint.dev/kachick/dprint-plugin-kdl/latest.json".to_string(),
+                ),
             },
             file_matching: FileMatchingInfo {
                 file_extensions: vec!["kdl".to_string()],

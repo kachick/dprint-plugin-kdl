9ecfa6979c8284c7bff0ff5e38c8f97836af0223
Kenichi Kamiya <kachick1@gmail.com>
2025-05-13 13:33:35 +0900

    Refine flake (#143)
    * Refine flake
    
    * Fix wrong falsy syntax in YAML

--- flake.nix
+++ flake.nix
@@ -1,47 +1,49 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-    unstable-nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs =
     {
       self,
       nixpkgs,
-      unstable-nixpkgs,
     }:
     let
       lib = nixpkgs.lib;
       forAllSystems = lib.genAttrs lib.systems.flakeExposed;
     in
     {
-      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixfmt-rfc-style);
+      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixfmt-tree);
       devShells = forAllSystems (
         system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
-          unstables = unstable-nixpkgs.legacyPackages.${system};
         in
         {
           default = pkgs.mkShell {
-            buildInputs =
-              with pkgs;
-              [
-                bashInteractive
-                findutils # xargs
-                nixfmt-rfc-style
-                nil
-                go-task
 
-                typos
+            env = {
+              # Fix nixd pkgs versions in the inlay hints
+              NIX_PATH = "nixpkgs=${pkgs.path}";
+              # For vscode typos extension
+              TYPOS_LSP_PATH = lib.getExe pkgs.typos-lsp;
+            };
 
-                rustc
-                cargo
-                rustfmt
-                rust-analyzer
-                clippy
-              ]
-              ++ [ unstables.dprint ];
+            buildInputs = with pkgs; [
+              bashInteractive
+              findutils # xargs
+              nixfmt-rfc-style
+              nixd
+              go-task
+              typos
+
+              dprint
+              rustc
+              cargo
+              rustfmt
+              rust-analyzer
+              clippy
+            ];
 
             nativeBuildInputs = with pkgs; [
               rustc-wasm32.llvmPackages.bintools # rust-lld

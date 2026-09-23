f625b09af7ccb8d34d4d35c76300f4f7e05bd327
Kenichi Kamiya <kachick1@gmail.com>
2026-09-24 03:10:57 +0900

    Replace nixfmt and nixfmt-tree with kachick/dprint-plugin-nix

--- flake.nix
+++ flake.nix
@@ -13,7 +13,21 @@
       forAllSystems = lib.genAttrs lib.systems.flakeExposed;
     in
     {
-      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixfmt-tree);
+      formatter = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        pkgs.writeShellApplication {
+          name = "dprint-fmt";
+          runtimeInputs = with pkgs; [
+            dprint
+          ];
+          text = ''
+            dprint fmt "$@"
+          '';
+        }
+      );
 
       packages = forAllSystems (
         system:
@@ -40,8 +54,6 @@
               bashInteractive
               findutils # xargs
               diffutils # for E2E test
-              nixfmt
-              nixfmt-tree
               nixd
               go-task
               typos
